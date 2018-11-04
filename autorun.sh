4aa690810990da0dd7cb01c7aa1c1a113dcb1c23
magicdominic <2743621+magicdominic@users.noreply.github.com>
2018-11-04 16:58:33 +0100

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+cd /home/pi/labscreen/
 while [[ True ]]; do
     #statements
-    python3 /home/pi/labscreen/main.py
+    python3 main.py
     if [[ $? -eq 0 ]]; then
         echo "application exited cleanly, not restarting"
         break
